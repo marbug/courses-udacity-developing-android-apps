d36ba8b73fda49d5e0d9a8b538df97d98415402c
Maryan Bahnyuk <marbuga@gmail.com>
2017-05-21 12:58:14 +0300

    Inserts With ContentProvider

--- course-app/Sunshine/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ course-app/Sunshine/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -28,7 +28,7 @@ import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     static final String DATABASE_NAME = "weather.db";
 
