fd357191aa7bcd1a77131f3d732cbd2f1eae3091
Maryan Bahnyuk <marbuga@gmail.com>
2017-05-23 16:43:58 +0300

    Today Item On Tablet

--- course-app/Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ course-app/Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -45,6 +45,10 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         } else {
             mTwoPane = false;
         }
+
+        ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_forecast));
+        forecastFragment.setUseTodayLayout(!mTwoPane);
     }
 
     @Override
