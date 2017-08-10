a60decf78829342cff1b72d93be6e31bfef72948
Maryan Bahnyuk <marbuga@gmail.com>
2017-08-10 12:29:00 +0300

    add adapter

--- course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/PlaceholderFragment.java
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/PlaceholderFragment.java
@@ -5,12 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public PlaceholderFragment() {
     }
@@ -21,15 +24,23 @@ public class PlaceholderFragment extends Fragment {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
         String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
         };
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter =
+            new ArrayAdapter<String>(
+                getActivity(), // The current context (this activity)
+                R.layout.list_item_forecast, // The name of the layout ID.
+                R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                weekForecast);
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
