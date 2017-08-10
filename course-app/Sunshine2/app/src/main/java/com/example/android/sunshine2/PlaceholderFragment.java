f332399af7bdacbe728e29b6e1063cdab55ecb72
Maryan Bahnyuk <marbuga@gmail.com>
2017-08-10 12:07:58 +0300

    add and use PlaceholderFragment

---
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/PlaceholderFragment.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
