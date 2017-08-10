37c584d18d2fb50cdc81e354e593994278c2a413
Maryan Bahnyuk <marbuga@gmail.com>
2017-08-10 12:25:49 +0300

    add fake data

--- course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/MainActivity.java
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/MainActivity.java
@@ -16,4 +16,6 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
     }
+
+
 }
--- course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/PlaceholderFragment.java
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/PlaceholderFragment.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PlaceholderFragment extends Fragment {
 
     public PlaceholderFragment() {
@@ -14,6 +18,18 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
