cff543f615e848f3afe0219fd873c44f4ad1990a
Maryan Bahnyuk <marbuga@gmail.com>
2017-08-10 15:49:30 +0300

    use location from settings

--- course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/ForecastFragment.java
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.android.sunshine2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import android.support.v4.app.Fragment;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -59,7 +62,11 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            Toast.makeText(getActivity(), location, Toast.LENGTH_SHORT).show();
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
