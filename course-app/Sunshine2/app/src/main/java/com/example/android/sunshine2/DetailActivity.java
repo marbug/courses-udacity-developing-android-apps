a602c19603a8203b6abc033794ff325d7ecda6e3
Maryan Bahnyuk <marbuga@gmail.com>
2017-08-14 19:00:47 +0300

    use projections (column indexes)

--- course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/DetailActivity.java
+++ course-app/Sunshine2/app/src/main/java/com/example/android/sunshine2/DetailActivity.java
@@ -75,6 +75,9 @@ public class DetailActivity extends AppCompatActivity {
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            }
+
+            if (null != mForecastStr) {
                 ((TextView) rootView.findViewById(R.id.detail_text))
                         .setText(mForecastStr);
             }
