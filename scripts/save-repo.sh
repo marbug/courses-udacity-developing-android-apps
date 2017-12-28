832b665e2b1a4d078f727117e2c293e133c71900
Maryan Bagnyuk <marbuga@gmail.com>
2017-12-28 11:52:23 +0200

    enhance

--- scripts/save-repo.sh
+++ scripts/save-repo.sh
@@ -5,10 +5,10 @@ vCurrentDir=`dirname $0`
 echo $vCurrentDir
 cd $vCurrentDir
 
-echo "==== Save to bitbucket ..."
-./use-bitbucket.sh
-git push
-
 echo "==== Save to github ..."
 ./use-github.sh
+git push
+
+echo "==== Save to bitbucket ..."
+./use-bitbucket.sh
 git push
