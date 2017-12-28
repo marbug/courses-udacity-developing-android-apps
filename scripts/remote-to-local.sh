ee418aa18c66e7d7c0cb06bcdccf95ed2f2a755e
Maryan Bagnyuk <marbuga@gmail.com>
2017-12-28 11:54:52 +0200

    enhance

--- scripts/remote-to-local.sh
+++ scripts/remote-to-local.sh
@@ -6,3 +6,4 @@ git checkout master
 git pull
 git branch | grep -v 'master' | awk '{print "git branch -D "$1;}' | sh
 git branch -r | grep 'origin/sunshine' | awk '{print "git checkout " substr($1, 8)}' | sh
+git checkout master
